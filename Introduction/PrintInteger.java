039905210f85406bf7109abcb16f44e0a1ef6d34
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-12 12:33:09 +0530

    Printing a User Input to console

---
+++ Introduction/PrintInteger.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class PrintInteger {
+    public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a No. :");
+        int a = scan.nextInt();
+        System.out.println("Input :" + a);
+        scan.close();
+    }
+}
