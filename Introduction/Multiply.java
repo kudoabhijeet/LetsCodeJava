88c8f257e4d3ead264363f05b6ad1cebd89c61f0
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-12 12:44:25 +0530

    Product of two no.s

---
+++ Introduction/Multiply.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Multiply {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first :");
+        float a = sc.nextFloat();
+        System.out.print("Enter second :");
+        float b = sc.nextFloat();
+        System.out.println("Product : " + a*b);
+    }
+}
