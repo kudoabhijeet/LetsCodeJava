19fc6e769d70df9ab4183a0cc04779e6607580dd
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-14 02:45:40 +0530

    Leap Year

---
+++ Decision Making/LeapYear.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a year : ");
+        int year = scan.nextInt();
+        boolean leap = false;
+
+        if(year % 4 == 0)
+        {
+            if( year % 100 == 0)
+            {
+                if ( year % 400 == 0)
+                    leap = true;
+                else
+                    leap = false;
+            }
+            else
+                leap = true;
+        }
+        else
+            leap = false;
+
+        if(leap)
+            System.out.println(year + " is a leap year.");
+        else
+            System.out.println(year + " is not a leap year.");
+    }
+}
