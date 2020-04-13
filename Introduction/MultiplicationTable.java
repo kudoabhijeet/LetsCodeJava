d8c7669be1157cf03bcdbe36dc080cab14601068
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-14 02:17:33 +0530

    Multiplication Table

---
+++ Introduction/MultiplicationTable.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a No. : ");
+        int n = scan.nextInt();
+        for (int i=1;i<=10;i++){
+            System.out.println(n + " x " + i + " = " + n*i);
+        }
+
+    }
+}
