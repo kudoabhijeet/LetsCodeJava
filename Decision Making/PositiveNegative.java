4df6899cf71f99ee81205337105b9259e9841862
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-15 09:52:03 +0530

    Positive or Negative Check

---
+++ Decision Making/PositiveNegative.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class PositiveNegative {
+    public static void main(String[] args) {
+        double num;
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextDouble();
+        if (num < 0.0) {
+            System.out.println("Negative!");
+        }
+        else{
+            System.out.println("Negative!");
+        }
+        sc.close();
+    }
+}
