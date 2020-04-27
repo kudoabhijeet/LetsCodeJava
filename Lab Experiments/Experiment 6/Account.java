87aa8e7864ecca7cfe7c7ed039ba7ad232f9e278
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-28 02:46:18 +0530

    Package for Exp6_2

---
+++ Lab Experiments/Experiment 6/Account.java
@@ -0,0 +1,9 @@
+package Balance;
+public class Account {
+    public void displayBalance(){
+        System.out.println("Current Balance :");
+    }
+    public static void main(String[] args) {
+        System.out.println("Balance Package");
+    }
+}
