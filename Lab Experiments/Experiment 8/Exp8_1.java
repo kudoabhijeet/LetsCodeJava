246422fa21c2e7c7d17e9d01457cd108d16904cc
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-30 16:34:41 +0530

    Experiment 8_1

---
+++ Lab Experiments/Experiment 8/Exp8_1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Exp8_1 {
+    public static void main(String[] args) {
+        String str = "UPES DEVOPS";
+        int findex = str.indexOf("S");
+        int lindex = str.lastIndexOf("S");
+        System.out.println("First Index = " + findex);
+        System.out.println("First Index = " + lindex);
+
+    }
+}
