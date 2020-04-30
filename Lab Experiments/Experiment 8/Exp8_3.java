ab221b70865acc7d5608f20ff47dd04b43e9b811
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-30 17:09:38 +0530

    Experiment 8_3

---
+++ Lab Experiments/Experiment 8/Exp8_3.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.Scanner;
+public class Exp8_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = " ";
+        str = sc.nextLine();
+        System.out.println(str.toUpperCase());
+        sc.close();
+    }
+}   
