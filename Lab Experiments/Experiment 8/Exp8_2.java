426cf83e255bbbeffcf4476b0c27118d94a10a0b
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-30 16:55:38 +0530

    Experiment 8_2

---
+++ Lab Experiments/Experiment 8/Exp8_2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Exp8_2 {
+    public static void main(String[] args) {
+        String str = "upes devops";
+        String res = " ";
+        res = str.toUpperCase();
+        System.out.println(res);
+    }
+}
